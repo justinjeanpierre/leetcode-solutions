0d4fd3a893583fbdf2779773f99afebf0f25aa1b
Justin Jean-Pierre <jstn@jpdgtl.tech>
2025-07-01 15:29:13 -0400

    Initial Commit

---
+++ LeetCode Solutions/LeetCode_SolutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCode_SolutionsApp.swift
+//  LeetCode Solutions
+//
+//  Created by Justin Jean-Pierre on 2025-07-01.
+//
+
+import SwiftUI
+
+@main
+struct LeetCode_SolutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeetCode SolutionsTests/LeetCode_SolutionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCode_SolutionsTests.swift
+//  LeetCode SolutionsTests
+//
+//  Created by Justin Jean-Pierre on 2025-07-01.
+//
+
+import Testing
+@testable import LeetCode_Solutions
+
+struct LeetCode_SolutionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
